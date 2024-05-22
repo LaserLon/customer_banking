5b81c001c0636d4f7c2e212d91f2036dba4afe58
LaserLon <lonnie.aldredge@gmail.com>
2024-05-22 09:47:02 -0400

    fix limited decimal places to 2 when printing details

--- customer_banking.py
+++ customer_banking.py
@@ -10,7 +10,7 @@ def main():
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    savings_balance, savings_interest, savings_maturity = account_details()
+    savings_balance, savings_interest, savings_maturity = account_details("Savings account information")
     
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned =  create_savings_account(savings_balance, savings_interest, savings_maturity)
@@ -19,7 +19,7 @@ def main():
     print_details("Savings account interest", updated_savings_balance, interest_earned)
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
-    cd_balance, cd_interest, cd_maturity = account_details()
+    cd_balance, cd_interest, cd_maturity = account_details("CD account information")
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
@@ -37,15 +37,19 @@ def print_details(header, updated_balance, interest_earned):
         interest_earned (float): The interest earned on the account.
     """
     print (header)
-    print (f"Interest earned: {interest_earned}")
-    print (f"Current balance: {updated_balance}\n")
+    print (f"Interest earned: {interest_earned:.2f}")
+    print (f"Current balance: {updated_balance:.2f}\n")
         
-def account_details():
+def account_details(header):
     """Prompts the user to enter account details.
+
+    Args:
+        header (str): The header for the output.
 
     Returns:
         the balance (float), interest rate (float), and maturity (int).
     """
+    print(header)
     balance = input_num("Enter current balance: ")
     interest = input_num("Enter current interest rate: ")
     maturity = input_num("Enter total number of months: ")
